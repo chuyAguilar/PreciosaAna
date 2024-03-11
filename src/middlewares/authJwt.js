99fffc3e30847633be15c1c1831321a5377cc0c2
chuy <jesusaguilar32265@gmail.com>
2024-03-11 13:40:03 -0600

    recurso6 Middleware Roles

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -1,4 +1,5 @@
 import User from '../models/User';
+import Role from '../models/Role';
 import { verify_Token } from '../controllers/token.controller';
 
 
@@ -25,3 +26,39 @@ export const verifyToken = async (req, res, next) => {
     }
 };
 
+//validamos si es admin o si es moderador
+
+
+
+export const isAdmin = async (req,res,next) => {
+    //Busca el usuario en la base de datos
+    const user = await User.findById(req.userId);
+    //Buscar los roles de el ususario
+    const roles = await Role.find({ _id: {$in: user.roles} });
+    //console.log(roles);
+
+    //Recorrer roles de ususario
+    for(let i =0; i < roles.length; i++){
+        if(roles[i].name == "admin"){
+                next();
+                return;
+
+        }
+    }
+    
+    return res.status(403).json({message: "Requiere ser administrador"});
+}
+
+export const isModandAd = async (req, res, next) => {
+
+const user = await User.findById(req.userId);
+
+const roles = await Role.find({ _id: {$in: user.roles}});
+for(let i=0; i < roles.length; i ++){
+    if(roles[i].name == "moderator" || roles[i].name == "admin"){
+        next();
+        return;
+    }
+}
+
+}
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -2,12 +2,16 @@ import { Router } from "express";
 const router = Router();
 
 import * as productCTRL from '../controllers/products.controller';
-import { verifyToken } from "../middlewares/authJwt";
+import { authJwt } from "../middlewares/index";
 
 router.get('/',productCTRL.getProducts);
-router.post('/',verifyToken,productCTRL.createProduct);
 router.get('/:producID', productCTRL.getProductsbyid);
-router.put('/:productId', verifyToken, productCTRL.updateProduct);
-router.delete('/:productId',verifyToken, productCTRL.deleteProduct);
+
+//Rutas protegida solo para administrador
+router.post('/',[authJwt.verifyToken,authJwt.isAdmin],productCTRL.createProduct);
+
+//Rutas protegidas para administrador y moderador
+router.put('/:productId',[authJwt.verifyToken,authJwt.isModandAd], authJwt.verifyToken, productCTRL.updateProduct);
+router.delete('/:productId',[authJwt.verifyToken,authJwt.isModandAd],authJwt.verifyToken, productCTRL.deleteProduct);
 
 export default router;
