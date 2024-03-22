643c9598db79dfbbd3d4e690cff834fa1f96d3a0
chuy <jesusaguilar32265@gmail.com>
2024-03-22 01:03:26 -0600

    agregue problemas con babel en local

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,5 @@
 module.exports = {
-    presets: [
-      '@babel/preset-env',
-      '@babel/preset-react'
-    ],
-    plugins: [
-      '@babel/plugin-transform-runtime'
-    ],
-    // Directorio de salida configurado aquí
-    // Asegúrate de que coincida con lo que espera Vercel
-    outDir: 'build'
-  };
-  
+  presets: [
+    '@babel/preset-env'
+  ]
+};
